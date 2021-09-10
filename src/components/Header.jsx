52736ed1598579521bbe64f8384c9eb71a421714
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-11 03:51:56 +0700

    update, menambahkan menu pada sidebar, dan meng-upadet responsifitas pada header dan sidebar, menyesuaikan ukuran button pada SidebaBtn.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,19 @@ import image from '../img/profile.jpg'
 import { Theme } from '../utils/Theme'
 
 export default function Header() {
-  const { handleNotifbar, notifbar, profileMenu, handleProfileMenu } =
-    useContext(Theme)
+  const {
+    handleNotifbar,
+    notifbar,
+    profileMenu,
+    handleProfileMenu,
+    dummyText,
+    header,
+  } = useContext(Theme)
 
-  console.log(notifbar)
-  let dummyText = [
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi itaque error dolorum reprehenderit. Numquam laudantium quidem perspiciatis earum rerum nam debitis. Incidunt, non?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi itaque error dolorum reprehenderit. Numquam laudantium quidem perspiciatis earum rerum nam debitis. Incidunt, non?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi itaque error dolorum reprehenderit. Numquam laudantium quidem perspiciatis earum rerum nam debitis. Incidunt, non?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci pariatur laborum ducimus ea ex rem sed quia aliquam?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci pariatur laborum ducimus ea ex rem sed quia aliquam?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci pariatur laborum ducimus ea ex rem sed quia aliquam?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci pariatur laborum ducimus ea ex rem sed quia aliquam?',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam et natus sint soluta animi, inventore, nulla, quam alias saepe nihil optio aut. Magni blanditiis aliquid dignissimos officia sequi labore nihil autem sint?',
-  ]
   return (
-    <header className='flex items-center justify-between fixed top-0 w-full h-12 sm:h-14 md:h-16 px-2 sm:px-4 md:px-6 lg:pl-76 lg:pr-4 bg-white z-40'>
+    <header
+      ref={header}
+      className='flex items-center justify-between fixed top-0 w-full h-12 sm:h-14 md:h-16 px-2 sm:px-4 md:px-6 md:pl-14 lg:pr-4 bg-white z-40'>
       <div className='relative hidden md:flex items-center h-8 sm:h-9 md:h-10 w-full md:w-4/5 lg:w-3/4 rounded-md focus-within:shadow-md bg-gray-100 focus-within:bg-white'>
         <div className='grid place-items-center w-12 h-full text-gray-500'>
           <i className='bx bx-search'></i>
@@ -51,7 +48,7 @@ export default function Header() {
               Notifications
             </h5>
           </div>
-          <ul className='w-full py-1 h-full overflow-y-scroll'>
+          <ul className='w-full py-1 h-56 md:h-64 lg:h-80 overflow-y-scroll'>
             {dummyText.map((text, i) => (
               <li className='w-full py-2 border px-2 md:px-4' key={i}>
                 {text}
@@ -67,29 +64,29 @@ export default function Header() {
 
         <div
           ref={profileMenu}
-          className='absolute top-12 sm:top-14 md:top-16 w-full lg:w-1/3 h-0 shadow-md rounded-md overflow-hidden bg-white'>
+          className='absolute top-12 sm:top-14 md:top-16 w-full lg:w-1/3 h-42 lg:h-48 transform scale-0 -translate-y-1/2 shadow-md rounded-md overflow-hidden bg-white'>
           <nav className='w-full h-full flex flex-col items-stretch justify-between'>
             <a
               href='#'
-              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 hover:bg-gray-100'>
+              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 text-xs sm:text-sm font-medium hover:bg-gray-100'>
               <i className='bx bx-user'></i>
               <span>profile</span>
             </a>
             <a
               href='#'
-              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 hover:bg-gray-100'>
+              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 text-xs sm:text-sm font-medium hover:bg-gray-100'>
               <i className='bx bx-cog'></i>
               <span>setting</span>
             </a>
             <a
               href='#'
-              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 hover:bg-gray-100'>
+              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 text-xs sm:text-sm font-medium hover:bg-gray-100'>
               <i className='bx bx-support'></i>
               <span>help</span>
             </a>
             <a
               href='#'
-              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 hover:bg-gray-100'>
+              className='w-full h-full flex items-center space-x-2 px-2 md:px-4 py-2 text-xs sm:text-sm font-medium hover:bg-gray-100'>
               <i className='bx bx-log-out-circle'></i>
               <span>logout</span>
             </a>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,53 @@ import React, { useContext } from 'react'
 import { Theme } from '../utils/Theme'
 
 export default function Sidebar() {
-  const { sidebar } = useContext(Theme)
+  const {
+    aside,
+    asideExpander,
+    handleAsideExpander,
+    expandText,
+    dummyMenu,
+  } = useContext(Theme)
+
   return (
     <>
       <aside
-        ref={sidebar}
-        className='fixed left-0 top-0 w-10/12 xs:w-9/12 sm:w-8/12 md:w-72 h-screen transform -translate-x-full lg:transform-none bg-gray-900 z-50'></aside>
+        ref={aside}
+        className='group fixed left-0 top-0 w-10/12 xs:w-9/12 sm:w-8/12 md:w-12 md:hover:w-60 h-screen transform -translate-x-full md:transform-none overflow-x-hidden bg-gray-900 z-50'>
+        <nav className='relative flex flex-col justify-between w-full h-full'>
+          <ul className='w-full flex flex-col items-stretch pt-12 max-h-96 overflow-y-hidden overflow-x-hidden'>
+            {dummyMenu.map((menu, i) => (
+              <li key={i}>
+                <a
+                  href='#'
+                  className='relative flex items-center w-full text-sm md:text-base h-12 whitespace-nowrap text-gray-200 hover:bg-gray-800'>
+                  <span className='grid place-items-center w-10 h-10 md:w-12 md:h-12 absolute left-0'>
+                    <i className={`bx ${menu.icon}`}></i>
+                  </span>
+                  <span className='ml-12 md:ml-14 font-medium text-base'>
+                    {menu.text}
+                  </span>
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className='hidden md:block mt-auto'>
+            <button
+              onClick={handleAsideExpander}
+              className='relative flex items-center w-full h-12 text-gray-200 hover:bg-gray-800'>
+              <span className='grid place-items-center w-12 h-12 absolute left-0'>
+                <i
+                  ref={asideExpander}
+                  className='bx bx-chevrons-right transform group-hover:rotate-180'></i>
+              </span>
+              <span
+                className='ml-14 font-medium text-base'
+                ref={expandText}></span>
+            </button>
+          </div>
+        </nav>
+      </aside>
     </>
   )
 }
