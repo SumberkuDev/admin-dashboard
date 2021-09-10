6f82c444cd3c57179652203c66902c60b58fa383
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-11 06:15:56 +0700

    memperbaiki Sidebar yang muncul ketika breakpoint menyentuh 'md'

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,7 +14,7 @@ export default function Sidebar() {
     <>
       <aside
         ref={aside}
-        className='group fixed left-0 top-0 w-10/12 xs:w-9/12 sm:w-8/12 md:w-12 md:hover:w-60 h-screen transform -translate-x-full md:transform-none overflow-x-hidden bg-gray-900 z-50'>
+        className='group fixed left-0 top-0 w-10/12 xs:w-9/12 sm:w-2/5 lg:w-12 lg:hover:w-60 h-screen transform -translate-x-full lg:transform-none overflow-x-hidden bg-gray-900 z-50'>
         <nav className='relative flex flex-col justify-between w-full h-full'>
           <ul className='w-full flex flex-col items-stretch pt-12 max-h-96 overflow-y-hidden overflow-x-hidden'>
             {dummyMenu.map((menu, i) => (
@@ -33,7 +33,7 @@ export default function Sidebar() {
             ))}
           </ul>
 
-          <div className='hidden md:block mt-auto'>
+          <div className='hidden lg:block mt-auto'>
             <button
               onClick={handleAsideExpander}
               className='relative flex items-center w-full h-12 text-gray-200 hover:bg-gray-800'>
