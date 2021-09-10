76fba72fa37f96c4f005ee15330f16e926b924f7
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2021-09-11 06:16:43 +0700

    update, memperbaiki dimana button menghilang pada media breakpoint 'md'

--- src/components/SidebarBtn.jsx
+++ src/components/SidebarBtn.jsx
@@ -7,7 +7,7 @@ export default function SidebarBtn() {
     <button
       onClick={handleAside}
       ref={asideHandler}
-      className='grid md:hidden place-items-center fixed right-3 md:right-4 bottom-3 md:bottom-4 w-8 sm:w-10 h-8 sm:h-10 text-lg sm:text-xl rounded-xl text-white bg-gray-800'>
+      className='grid lg:hidden place-items-center fixed right-3 md:right-4 bottom-3 md:bottom-4 w-8 sm:w-10 h-8 sm:h-10 text-lg sm:text-xl rounded-xl text-white bg-gray-800'>
       <i className='bx bx-menu' ref={hamburger}></i>
     </button>
   )
